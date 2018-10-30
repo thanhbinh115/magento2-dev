4dbbc22baa8962c59b37cebf6ec9d6297ee757e0
thanhbinh <thanhbinh11051985@gmail.com>
2018-10-30 11:39:38 +0700

    + Add SAI theme standard

--- app/design/frontend/Sai/standard/registration.php
+++ app/design/frontend/Sai/standard/registration.php
@@ -3,10 +3,10 @@
  * Created by PhpStorm.
  * User: thanhbinh
  * Date: 2018/10/29
- * Time: 3:47 PM
+ * Time: 4:58 PM
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Sai/basic',
+    'frontend/Sai/standard',
     __DIR__
 );
